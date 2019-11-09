b1321d31635fc7b881e4023fa7968e2298bda836
ljj_home <1792539263@qq.com>
2019-11-09 14:03:59 +0800

    文件内容替换

--- com/bjsxt/python/fileIo/regularFile.py
+++ com/bjsxt/python/fileIo/regularFile.py
@@ -1,21 +1,70 @@
 import re
+import random
 print('hello world')
 
+def intTOstr(a):
+    return str(a)
 
-str = ''
+def getName():
+    # 姓
+    arr = ['赵', '王', '孙', '李', '郭', '农', '徐', '陈', '秦', '张', '钱', '高', '江', '刘', '孔']
+    randomNum = random.randint(0, arr.__len__() - 1)
+    firstName = arr[randomNum]
+    return firstName
+
+def rePlaceStr( line ):
+    print('input str is :' + line)
+    if line.__contains__('_id'):
+        str = re.search(r'("_id" : ")(\d+)(",)', line, re.M | re.I)
+        id = str.group(2)
+        randNum = random.randint(0, 1000000)
+        line = line.replace(id, intTOstr(randNum))
+        print('result str is :'+line)
+        return line
+    if line.__contains__('userRealname'):
+        str = re.search(r'("userRealname" : ")(.*)(",)', line, re.M | re.I)
+        userRealname = str.group(2)
+
+        line = line.replace(userRealname, getName())
+        print('result str is :' + line)
+        return line
+    if line.__contains__('userIdcardNo'):
+        str = re.search(r'("userIdcardNo" : ")(\d+)(")', line, re.M | re.I)
+        userIdcardNo = str.group(2)
+        randNum = random.randint(0, 1000000)
+        line = line.replace(userIdcardNo,intTOstr(randNum))
+        print('result str is :' + line)
+        return line
+
+resultStr = ''
+
+
+resultFile = open('./resultFile.json', 'w', encoding='utf8')
 with open('test.json', 'r',encoding='utf8') as lines:
     for line in lines:
+        tempStr = line
         if line.__contains__('_id'):
-            print(line)
+            tempStr = rePlaceStr(line)
 
+        if line.__contains__('userRealname'):
+            tempStr = rePlaceStr(line)
+        if line.__contains__('userIdcardNo'):
+            tempStr = rePlaceStr(line)
+        resultStr = resultStr + tempStr
+        # resultFile.write(resultStr)
         pass
 
+    # 打印输出结果
+    print(resultStr)
 
-line = '		"_id" : "61",'
+    resultFile.writelines(resultStr)
+    resultFile.close()
 
-str = re.search(r'("_id" : ")(\d+)(",)', line, re.M|re.I)
-print(str.group(0))
-print(str.group(1))
-print(str.group(2))
-print(str.group(3))
+
+
+
+
+
+
+
 
