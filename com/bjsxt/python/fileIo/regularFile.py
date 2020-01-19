89a6dd49f8510205fdee81e8285a9695045773d3
ljj_home <1792539263@qq.com>
2020-01-19 22:16:43 +0800

    替换

--- com/bjsxt/python/fileIo/regularFile.py
+++ com/bjsxt/python/fileIo/regularFile.py
@@ -14,15 +14,20 @@ def getName():
 
 def rePlaceStr( line ):
     print('input str is :' + line)
+    if line.__contains__('"_id" : ObjectId'):
+        print('result str is :'+line)
+        return line
     if line.__contains__('_id'):
-        str = re.search(r'("_id" : ")(\d+)(",)', line, re.M | re.I)
+        str = re.search(r'("_id" : ")(.*)(",)', line, re.M | re.I)
         id = str.group(2)
         randNum = random.randint(0, 1000000)
         line = line.replace(id, intTOstr(randNum))
         print('result str is :'+line)
         return line
     if line.__contains__('userRealname'):
-        str = re.search(r'("userRealname" : ")(.*)(",)', line, re.M | re.I)
+        print(">>>>>>>>>>>"+line)
+        str = re.search(r'("userRealname" : ")(.*)(")', line, re.M | re.I)
+        print(str)
         userRealname = str.group(2)
 
         line = line.replace(userRealname, getName())
@@ -35,11 +40,35 @@ def rePlaceStr( line ):
         line = line.replace(userIdcardNo,intTOstr(randNum))
         print('result str is :' + line)
         return line
-
+    if line.__contains__('Name'):
+        str = re.search('("Name" : )(.*)(")', line, re.M | re.I)
+        Name = str.group(2)
+        line = line.replace(Name, "未知")
+        print('result str is :' + line)
+    if line.__contains__('TelephoneNo'):
+        str = re.search('("TelephoneNo" : )(.*)(")', line, re.M | re.I)
+        TelephoneNo = str.group(2)
+        line = line.replace(TelephoneNo, "12345678901")
+        print('result str is :' + line)
+    if line.__contains__('Certno'):
+        str = re.search('("Certno" : )(.*)(")', line, re.M | re.I)
+        Certno = str.group(2)
+        line = line.replace(Certno, "12345678901")
+        print('result str is :' + line)
+    if line.__contains__('Certtype'):
+        str = re.search('("Certtype" : )(.*)(")', line, re.M | re.I)
+        Certtype = str.group(2)
+        line = line.replace(Certtype, "未知")
+        print('result str is :' + line)
+    if line.__contains__('Employer'):
+        str = re.search('("Employer" : )(.*)(")', line, re.M | re.I)
+        Employer = str.group(2)
+        line = line.replace(Employer, "未知")
+        print('result str is :' + line)
 str1 = '"userIdcardNo" : "26X"'
 
-str1 = rePlaceStr(str1)
-print(str1)
+# str1 = rePlaceStr(str1)
+# print(str1)
 
 
 
@@ -47,7 +76,7 @@ resultStr = ''
 
 
 resultFile = open('./resultFile.json', 'w', encoding='utf8')
-with open('test.json', 'r',encoding='utf8') as lines:
+with open('test.txt', 'r', encoding='utf8') as lines:
     for line in lines:
         tempStr = line
         if line.__contains__('_id'):
@@ -56,6 +85,7 @@ with open('test.json', 'r',encoding='utf8') as lines:
         if line.__contains__('userRealname'):
             tempStr = rePlaceStr(line)
         if line.__contains__('userIdcardNo'):
+            print(line)
             tempStr = rePlaceStr(line)
         resultStr = resultStr + tempStr
         # resultFile.write(resultStr)
